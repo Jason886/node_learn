4478c6a6371f6d6d74b5b2127e39a88507a45f3c
Jason886 <359152155@qq.com>
2018-04-17 13:32:54 +0800

    hello world

---
+++ 01_hello/01_hello.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+function _server_handle(request, response) {
+    // 发送HTTP头部
+    // HTTP状态值: 200 OK
+    // 内容类型: text/plain
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    // 发送响应数据 "Hello World"
+    response.end('Hello World\n');
+}
+
+var server = http.createServer(_server_handle);
+server.listen(8888);
+console.log('Server running at http://127.0.0.1:8888');
